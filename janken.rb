143b01d0857f12112f83e18fad2cf1ea88027b11
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-06-18 13:47:20 +0900

    fix janken.rb

--- janken.rb
+++ janken.rb
@@ -3,14 +3,11 @@ class Player
   def hand
     puts "数字を入力してください。"
     puts "0:グー, 1:チョキ, 2:パー"
-
     input_hand = gets.chomp
     player_hand = input_hand.to_i
-
     while true
       if input_hand == "0" || input_hand == "1" || input_hand == "2"
       return player_hand
-
       else
         puts "もう一度数字を入力してください。"
         puts "0:グー, 1:チョキ, 2:パー"
@@ -20,7 +17,6 @@ class Player
     end
   end
 end
-
 class Enemy
   def hand
     random_hand = rand(3)
@@ -32,14 +28,14 @@ class Janken
     janken = ["グー", "チョキ", "パー"]
     puts "相手の手は#{janken[enemy_hand]}です"
       if player_hand == enemy_hand
-        puts "私：#{janken[player_hand]},相手：#{janken[enemy_hand]},よってあいこ"
+        puts "私の手は#{janken[player_hand]},よってあいこ"
         return true
       elsif
         (player_hand == 0 && enemy_hand == 1) || (player_hand == 1 && enemy_hand ==2) || (player_hand == 2 && enemy_hand == 0)
-        puts "私：#{janken[player_hand]},相手：#{janken[enemy_hand]},よってあなたの勝ちです"
+        puts "私の手は#{janken[player_hand]},よってあなたの勝ちです"
         return false
       else
-        puts "私：#{janken[player_hand]},相手：#{janken[enemy_hand]},よってあなたの負けです"
+        puts "私の手は#{janken[player_hand]},よってあなたの負けです"
         return false
       end
   end
@@ -47,7 +43,6 @@ end
 player = Player.new
 enemy = Enemy.new
 janken = Janken.new
-
 next_game = true
 while next_game
   next_game = janken.pon(player.hand, enemy.hand)
